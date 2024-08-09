db80bc043a6bc68e1cc811481645eea2920f37b4
adithyay19 <adithya842@gmail.com>
2024-08-10 01:27:34 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const data = await fetch("https://api.adviceslip.com/advice");
+    const response = await data.json();
+    console.log(response.slip.advice);
+    setAdvice(response.slip.advice);
+    setCount(count + 1);
+  }
+
+  useEffect(function () {
+    getAdvice();
+  }, []);
+
+  return (
+    <div>
+      <h3>{advice}</h3>
+      <button onClick={getAdvice}>Get another advice</button>
+    </div>
+  );
+}
